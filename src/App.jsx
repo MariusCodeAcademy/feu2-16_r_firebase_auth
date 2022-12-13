0f140c72f649c68e2a4d59fee9c7b75feca6f872
Marius <marius@example.com>
2022-12-13 14:05:14 +0200

    Protect routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,28 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import NotAuthorised from './pages/NotAuthorised';
+import { useAuthCtx } from './store/AuthContext';
 
 function App() {
   // App priklausomai nuo isUserLoggedIn generuoti routus
+  const { isUserLoggedIn } = useAuthCtx();
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+
+        {/* <ProtectedRoute path='/auth'>
           <AuthPage />
+        </ProtectedRoute> */}
+
+        <Route path='/auth'>
+          {!isUserLoggedIn ? <AuthPage /> : <NotAuthorised />}
         </Route>
         <Route path='/profile'>
-          <UserProfile />
+          {isUserLoggedIn ? <UserProfile /> : <NotAuthorised />}
         </Route>
       </Switch>
     </Layout>
