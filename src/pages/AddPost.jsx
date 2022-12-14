82c3f3e2bbaaaebc68ae0dcf5e2433a9b57237c6
Marius <marius@example.com>
2022-12-14 09:51:01 +0200

    VITE_REAL_DB_URL

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,14 +1,24 @@
 // values
 const dummyPost = {
-  title: '',
-  text: '',
+  image: 'https://picsum.photos/id/17/200/300',
+  title: 'First fireBase posts',
+  body: 'Fire base is almost easy',
+  userId: 1,
+  archived: false,
 };
 function AddPost(props) {
+  const handleNewPost = () => {
+    console.log('dummyPost ===', dummyPost);
+
+    const url = `${import.meta.env.VITE_REAL_DB_URL}/myApp/postai.json`;
+    console.log('url ===', url);
+  };
+
   return (
     <div className='container'>
       <h1>AddPost</h1>
       <p>cia bus forma</p>
-      <button>Create post</button>
+      <button onClick={handleNewPost}>Create post</button>
     </div>
   );
 }
